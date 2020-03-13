10c7c08ae47a161dee0ca942af5a34134a108f85
Muzlo <171361634@qq.com>
2020-03-13 20:48:18 +0800

    123

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,16 @@
-// module.exports = {
-//     // //lintOnSave: false,
-//     // devServer: {
-//     //     proxy: {
-//     //         '/fr': {
-//     //             target: 'http://192.168.2.193:38889/', // 对应自己的接口
-//     //             changeOrigin: true,
-//     //             ws: true,
-//     //             pathRewrite: {
-//     //                 '^/fr': '',
-//     //             },
-//     //         },
-//     //     },
-//     // },
-// }
+module.exports = {
+    // //lintOnSave: false,
+    // devServer: {
+    //     proxy: {
+    //         '/fr': {
+    //             target: 'http://192.168.2.193:38889/', // 对应自己的接口
+    //             changeOrigin: true,
+    //             ws: true,
+    //             pathRewrite: {
+    //                 '^/fr': '',
+    //             },
+    //         },
+    //     },
+    // },
+    publicPath: './'
+}
